079a03bca1fdecf7bcbd91b5e50254af875c1421
danilowhk <danilowhk@gmail.com>
2022-08-24 21:18:56 -0300

    setup commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: function (config, options) {
+    if (!options.isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    config.experiments = { asyncWebAssembly: true, layers :true };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ pages/qrcode.js
@@ -0,0 +1,80 @@
+// import "./styles.css";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+const QrReader = dynamic(() => import('react-qr-reader'), {
+  ssr: false
+  });
+// import QrReader from "react-qr-reader";
+import { Identity } from "@semaphore-protocol/identity";
+
+
+const App = () => {
+  const [selected, setSelected] = useState("environment");
+  const [startScan, setStartScan] = useState(false);
+  const [loadingScan, setLoadingScan] = useState(false);
+  const [data, setData] = useState("");
+  const [identity,setIdentity] = useState();
+
+
+  const handleScan = async (scanData) => {
+    setLoadingScan(true);
+    console.log(`loaded data data`, scanData);
+    if (scanData && scanData !== "") {
+      console.log(`loaded >>>`, scanData);
+      setData(scanData);
+      setStartScan(false);
+      setLoadingScan(false);
+      // setPrecScan(scanData);
+    }
+  };
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  const handleGenerateId = () => {
+    const newIdentity = new Identity(data);
+    const newIdentityCommitment = newIdentity.generateCommitment();
+    console.log(newIdentityCommitment);
+    setIdentity(newIdentityCommitment);
+  }
+
+  return (
+    <div className="App">
+    <h1>Hello World</h1>
+      <h2>
+        Last Scan:
+        {selected}
+      </h2>
+
+      <button
+        onClick={() => {
+          setStartScan(!startScan);
+        }}
+      >
+        {startScan ? "Stop Scan" : "Start Scan"}
+      </button>
+      {startScan && (
+        <>
+          <select onChange={(e) => setSelected(e.target.value)}>
+            <option value={"environment"}>Back Camera</option>
+            <option value={"user"}>Front Camera</option>
+          </select>
+          <QrReader
+            facingMode={selected}
+            delay={1000}
+            onError={handleError}
+            onScan={handleScan}
+            // chooseDeviceId={()=>selected}
+            style={{ width: "300px" }}
+          />
+        </>
+      )}
+      {loadingScan && <p>Loading</p>}
+      {data !== "" && <p>{data}</p>}
+      <button onClick={handleGenerateId}>Generate Id</button>
+      <h1>{identity}</h1>
+    </div>
+  );
+};
+
+export default App;
